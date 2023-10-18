4a57fd043afe6aaede62e76c12e6355960926f40
Jake Girhiny <jegirhiny@gmail.com>
2023-10-18 15:44:09 -0400

    Init commit with template

---
+++ app.js
@@ -0,0 +1,5 @@
+const apiKey = '9b60528ef675419baa2214630222305';
+const baseURL = 'http://api.weatherapi.com/v1/';
+let searchTerm = '';
+
+//http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchTerm}&aqi=yes`)
